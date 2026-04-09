e3580162d4d620840c1dd58ec668adfd89adc2c3
drissbejgana <bejganadriss@gmail.com>
2026-04-09 15:25:22 +0100

    change status

--- database/migrations/2026_04_09_141625_update_factures_status_values.php
+++ database/migrations/2026_04_09_141625_update_factures_status_values.php
@@ -9,16 +9,15 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-         DB::table('factures')->where('status', 'en_attente')->update(['status' => 'non_payee']);
-    }
+        public function up(): void
+        {
+            DB::statement("ALTER TABLE factures MODIFY COLUMN status ENUM('en_attente','non_payee','partielle','en_cours','payee','annulee') DEFAULT 'non_payee'");
+            DB::table('factures')->where('status', 'en_attente')->update(['status' => 'non_payee']);
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+        public function down(): void
+        {
             DB::table('factures')->where('status', 'non_payee')->update(['status' => 'en_attente']);
-    }
+            DB::statement("ALTER TABLE factures MODIFY COLUMN status ENUM('en_attente','payee','annulee') DEFAULT 'en_attente'");
+        }
 };
